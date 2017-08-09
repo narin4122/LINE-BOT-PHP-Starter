549a8b1e5a26197c456a6b78233733f170b9494b
Narin <narin4122@hotmail.co.th>
2017-08-09 13:53:08 +0700

    aa

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 
         // Get text sent
-        $text = $event['message']['text'] + " " +$event['message']['id']+ " " +$event['message']['type'];
+        $text = $event['message']['text'] + " /" +$event['message']['id']+ " /" +$event['message']['type'];
 
         // Get replyToken
         $replyToken = $event['replyToken'];
